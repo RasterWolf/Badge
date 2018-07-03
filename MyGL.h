8322c6b6b79c89f9e95863d2f9dd9c8a251ff6b4
Sakura <Sakura@DESKTOP-1JHHK06>
2018-07-02 21:51:13 -0500

    Disabel GL_ASSERT

--- MyGL.h
+++ MyGL.h
@@ -12,7 +12,7 @@
 #include "EGL/eglext.h"
 #endif
 
-#if 1
+#if 0
 #include <cassert>
 #include <iostream>
 #define GL_ASSERT {GLenum err = glGetError(); if(err != GL_NO_ERROR){std::cout<<"GL error: " << err << " Function: " << __FUNCTION__ <<std::endl; exit(0);}} 
