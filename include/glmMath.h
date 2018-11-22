b93174acfb133e197205d4665a0d677b8318780b
Sakura <Sakura@DESKTOP-1JHHK06>
2018-11-22 08:37:03 -0600

    Fixed needing to comment out conutof in vcos_types.h

--- include/glmMath.h
+++ include/glmMath.h
@@ -1,5 +1,8 @@
 ﻿#pragma once
 //matrix math (don't want to re-write this
+#if !defined(_MSC_VER)
+#undef countof
+#endif
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
